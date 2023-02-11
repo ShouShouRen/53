00515eae2028dacd746f8f040c6eb93aa9162a3b
Peter Chen <peterchen2017125@gmail.com>
2023-02-11 14:07:13 +0800

    20230211

--- login_check.php
+++ login_check.php
@@ -7,46 +7,6 @@ try {
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$user]);
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    // if (!$row["user"] !=$user || $row["pw"] != $pw) {
-    //     if (!isset($_SESSION["login_attempts"])) {
-    //         $_SESSION['login_attempts'] = 0;
-    //     }
-    //     $_SESSION["login_attempts"]++;
-    //     if ($_SESSION["login_attempts"] > 3) {
-    //         echo "連續誤錯 3 次";
-    //         unset($_SESSION["login_attempts"]);
-    //         header("Location: error.php");
-    //         return;
-    //     }
-    //     echo "帳號密碼錯誤";
-    //     header("Refresh:1;url=index.php");
-    //     // return;
-    // } else if (!$row["user"] !=$user) {
-    //     if (!isset($_SESSION["login_attempts"])) {
-    //         $_SESSION['login_attempts'] = 0;
-    //     }
-    //     $_SESSION["login_attempts"]++;
-    //     if ($_SESSION["login_attempts"] > 3) {
-    //         echo "連續誤錯 3 次";
-    //         unset($_SESSION["login_attempts"]);
-    //         header("Location: error.php");
-    //     }
-    //     echo "帳號錯誤";
-    //     // header("Refresh:1;url=index.php");
-    // } else if ($row["pw"] != $pw) {
-    //     if (!isset($_SESSION["login_attempts"])) {
-    //         $_SESSION['login_attempts'] = 0;
-    //     }
-    //     $_SESSION["login_attempts"]++;
-    //     if ($_SESSION["login_attempts"] > 3) {
-    //         echo "連續誤錯 3 次";
-    //         unset($_SESSION["login_attempts"]);
-    //         header("Location: error.php");
-    //     }
-    //     echo "密碼錯誤";
-    //     // header("Refresh:1;url=index.php");
-    //     // return;
-    // }
     function login_attempts()
     {
         if (!isset($_SESSION["login_attempts"])) {
