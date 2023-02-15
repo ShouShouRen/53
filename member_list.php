bc18bc64ac0646da3dc2ce6e98e1e17abac13da0
Peter Chen <peterchen2017125@gmail.com>
2023-02-15 21:51:24 +0800

    20230215

--- member_list.php
+++ member_list.php
@@ -27,7 +27,7 @@ try {
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="coffee.php">TODO</a>
+            <a class="navbar-brand" href="coffee.php">咖啡商品展示系統- 管理者頁面</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
