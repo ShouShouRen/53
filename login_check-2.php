4f18b569cfe039177d6dab20ffb8000ffb041932
Peter Chen <peterchen2017125@gmail.com>
2023-02-22 22:03:03 +0800

    20230222

--- login_check-2.php
+++ login_check-2.php
@@ -5,43 +5,58 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bootstrap.css">
     <title>Document</title>
     <style>
+        body {
+            background: #9e7676;
+            background: radial-gradient(circle, #9e7676 0%, #594545 100%);
+            height: 100vh;
+        }
+
         table {
             border-collapse: collapse;
         }
 
         td {
-            width: 50px;
-            height: 50px;
-            border: 1px solid black;
+            width: 180px;
+            height: 180px;
+            border: 2px solid #fff;
             text-align: center;
             font-size: 24px;
             cursor: pointer;
         }
 
         td.selected {
-            background-color: blue;
+            background-color: #fff;
+        }
+
+        .d-center {
+            position: absolute;
+            top: 50vh;
+            left: 50%;
+            transform: translate(-50%, -50%);
         }
     </style>
 </head>
 
 <body>
-    <table>
-        <tr>
-            <td data-id="1"></td>
-            <td data-id="2"></td>
-        </tr>
-        <tr>
-            <td data-id="3"></td>
-            <td data-id="4"></td>
-        </tr>
-    </table>
-
-    <p>請選擇兩個相鄰的格子，以連成一條水平或垂直線。</p>
-
-    <button id="validate">驗證</button>
-
+    <div class="container">
+        <div class="d-center text-center d-flex flex-column justify-content-center align-items-center">
+            <table>
+                <tr>
+                    <td data-id="1"></td>
+                    <td data-id="2"></td>
+                </tr>
+                <tr>
+                    <td data-id="3"></td>
+                    <td data-id="4"></td>
+                </tr>
+            </table>
+            <p class="py-3 m-0 text-white">請選擇兩個相鄰的格子，以連成一條水平或垂直線。</p>
+            <button id="validate" class="btn btn-outline-white">驗證</button>
+        </div>
+    </div>
 </body>
 <script src="./js/jquery-3.6.3.min.js"></script>
 <script>
