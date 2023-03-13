571bbf2cb87e3db2ad4800d2ff23ff9376373531
Peter Chen <peterchen2017125@gmail.com>
2023-03-13 21:46:07 +0800

    20230313

--- register_store.php
+++ register_store.php
@@ -12,16 +12,22 @@ if ($stmt_check->rowCount() > 0) {
     header("Refresh:1;url=member_list.php");
     return;
 }
-
-$sql_last_user = "SELECT id FROM users ORDER BY id DESC LIMIT 1";
-$stmt_last_user = $pdo->prepare($sql_last_user);
-$stmt_last_user->execute();
-$row_last_user = $stmt_last_user->fetch(PDO::FETCH_COLUMN, 0);
-$row_last_user + 1;
-$user_id = sprintf("%04d", $row_last_user);
-
 // 新增使用者
-$sql = "INSERT INTO users(user, user_name, pw, user_id) VALUES (?, ?, ?, ?)";
+$sql = "INSERT INTO users(user, user_name, pw) VALUES (?, ?, ?)";
 $stmt = $pdo->prepare($sql);
-$stmt->execute([$user, $user_name, $pw, $user_id]);
+if($stmt->execute([$user, $user_name, $pw])){
+    $last_id = $pdo->lastInsertId();
+    $user_id = sprintf('%04d', $last_id - 1);
+    $update_sql = "UPDATE users SET user_id = ? WHERE id = ?";
+    $stmt = $pdo->prepare($update_sql);
+    $stmt->execute([$user_id, $last_id]);
+}
+
+// $sql_last_user = "SELECT id FROM users ORDER BY id DESC LIMIT 1";
+// $stmt_last_user = $pdo->prepare($sql_last_user);
+// $stmt_last_user->execute();
+// $row_last_user = $stmt_last_user->fetch(PDO::FETCH_COLUMN, 0);
+// $row_last_user + 1;
+// $user_id = sprintf("%04d", $row_last_user);
+
 header("Refresh:1;url=login.php");
